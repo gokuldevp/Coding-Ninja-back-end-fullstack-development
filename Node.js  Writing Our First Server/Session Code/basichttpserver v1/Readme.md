cb20c1aa3e51483547f4c0b025748d45bf2d1bfd
Gokul Dev P <gokuldev.p123@gmail.com>
2023-07-15 14:54:28 +0530

    update 15-07-2023

--- Node.js  Writing Our First Server/Session Code/basichttpserver v1/Readme.md
+++ Node.js  Writing Our First Server/Session Code/basichttpserver v1/Readme.md
@@ -31,7 +31,7 @@ Step 5: check if the server is running in the browser for the given port
 localhost: port
 eg: http://127.0.0.1/:8000 
 
-======================================
+==================================================================================
 
 Handling Request and Responce
 Step 1: Create a new function with 2 argument - request and response.
@@ -48,3 +48,29 @@ let requestHandler = (request, response) => {
 ```
 
 Step 2: Add the new function as the parameter of the createServer function
+
+===================================================================================
+
+Reading and Writing Html file.
+We user the module fs for handling file.
+
+Step 1: Import the fs module
+```
+const fs = require("fs");
+```
+Step 2: Create a new function with 2 argument - request and response. for handling files
+
+Step 3: Add the path of the Html file
+```
+const path = "./.index.html"; 
+```
+Step 4: Using the fs module to handle the html file. - 2 arguments - path and function
+```
+fs.readFile(path, (err, data) => {
+    if(err) {
+        console.log(err);
+        return response.end("<h1>"+err+"</h1>")
+    }
+    return response.end(data);
+}); 
+``` 
--- Node.js  Writing Our First Server/Session Code/basichttpserver v1/index.js
+++ Node.js  Writing Our First Server/Session Code/basichttpserver v1/index.js
@@ -1,16 +1,17 @@
-const http = require("http");   //import the https module
+const http = require("http");   //import the http module
 const port = 8000;              //adding a port number
-
+const fs = require("fs");       // import fs module - user to read/write from files
 
 let requestHandler_1 = (request, response) => {
-    //Function to handle the server request and response
+    //Function to handle the server request and response when rendering text
+
     console.log(request.url);  // log the url part of the request, the area after the port
 
     response.end('Gotcha! 001');  //Print Gatcha on the screen
 }
 
 let requestHandler_2 = (request, response) => {
-    //Function to handle the server request and response
+    //Function to handle the server request and response when rendering html
     console.log(request.url);  // log the url part of the request, the area after the port
 
     // sets the status code to 200 (indicating a successful response) and specifies that the content being
@@ -22,7 +23,27 @@ let requestHandler_2 = (request, response) => {
     response.end('<h1>Hi Gokul</h1>');  
 }
 
-const server = http.createServer(requestHandler_2);  // createing http server request
+let requestHandler_3 = (request, response) => {
+    //Function to handle the server request and response when rendering html when html is in a seprate file
+    console.log(request.url);  // log the url part of the request, the area after the port
+
+    // sets the status code to 200 (indicating a successful response) and specifies that the content being
+    // sent back to the client is in HTML format. This header informs the client's web browser how to 
+    // interpret the data received from the server.
+    response.writeHead(200, {'content-type': 'text/html'});  
+
+    const path = "./index.html";    //Path of the Html file
+    fs.readFile(path, (err, data) => {
+        // Read the html file path, if the path exists render the html file content, else render the error
+        if(err) {
+            console.log(err);
+            return response.end("<h1>"+err+"</h1>")
+        }
+        return response.end(data);
+    }); 
+}
+
+const server = http.createServer(requestHandler_3);  // createing http server request
 
 server.listen(port, (err) => {
     if (err){
